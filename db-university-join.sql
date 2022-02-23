cb5fbfbe3b87e48c7fd0c00abf007f770c5921b9
ThubeesanKugathas <Thubeesan@outlook.com>
2022-02-23 16:07:15 +0100

    fix

--- db-university-join.sql
+++ db-university-join.sql
@@ -71,7 +71,9 @@ GROUP BY `teachers`.`id`;
 
 -- 7 BONUS: Selezionare per ogni studente quanti tentativi d'esame ha sostenuto per 
 -- superare ciascuno dei suoi esami
-SELECT `students`.`id`, `students`.`name`, `students`.`surname`, `exams`.`course_id`, `courses`.`name`, COUNT(*) AS 'given_exams'
+SELECT `students`.`id`, `students`.`name`, `students`.`surname`, 
+		`exams`.`course_id`, 
+		`courses`.`name`, COUNT(*) AS 'given_exams'
 FROM `students`
 JOIN `exam_student`
 	ON `students`.`id` = `exam_student`.`student_id`
